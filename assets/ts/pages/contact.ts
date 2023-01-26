e27d54c4109ee84e9cf6f670b7ce63e8bae4a0a6
zaypugh29 <123493030+zaypugh29@users.noreply.github.com>
2023-01-26 14:02:13 -0500

    Add files via upload

--- assets/ts/pages/contact.ts
+++ assets/ts/pages/contact.ts
@@ -9,9 +9,18 @@ export default async function main() {
 
         axios.post('/contacts', {
             // params and what
+            "name": (document.getElementById('name') as HTMLInputElement).value,
+            "company": (document.getElementById('company') as HTMLInputElement).value,
+            "email": (document.getElementById('email') as HTMLInputElement).value,
+            "phone": (document.getElementById('phone') as HTMLInputElement).value,
+            "content": "I'm not sure what this is yet", // from parameter body example
+            "referrer": "https://example.com/contact", // from parameter body example
+            "pageRequestID": "Fp5WjYYpIic", // from parameter body example
+            "tz": "America/New_York", // from parameter body example
         }).then(resp => {
             console.log('Success!');
             // redirect to success page
+            window.location.href = 'http://test.localhost:10506/contact-success/?';
         }).catch(err => alert(err));
     });
 }
